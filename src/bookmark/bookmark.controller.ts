bcff4c5879c48e4968b85323ef7b6ac032c5ce7a
ds3128 <dariussteves@yahoo.fr>
2023-08-06 21:17:16 +0100

    first nestJS app using prisma to manage dataBase

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get, HttpCode, HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post, Put,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from '../decorator';
+import {
+  CreateBookmarkDto,
+  EditBookmarkDto,
+} from './dto';
+
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(
+    private bookmarkService: BookmarkService,
+  ) {}
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ) {
+    return this.bookmarkService.createBookmark(
+      userId,
+      dto,
+    );
+  }
+  @Get()
+  getBookmark(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmark(
+      userId,
+    );
+  }
+
+  @Get(':id')
+  getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(
+      userId,
+      bookmarkId,
+    );
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmarkById(
+      userId,
+      bookmarkId,
+      dto,
+    );
+  }
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(
+      userId,
+      bookmarkId,
+    );
+  }
+}
